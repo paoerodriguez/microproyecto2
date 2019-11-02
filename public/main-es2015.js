6e00ee63e6c72b3f8d2fd4d552a63fd75a6ff59d
paoerodriguez <paolaerodriguezp@gmail.com>
2019-11-02 12:45:04 -0400

    cambios

--- public/main-es2015.js
+++ public/main-es2015.js
@@ -783,21 +783,20 @@ ReservarComponent = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "environment", function() { return environment; });
 /* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
 
 const environment = {
-    production: false
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyCjZKTo1WH-SVv0UWeJXLeLXSUW2cB6gRM",
+        authDomain: "micropro2.firebaseapp.com",
+        databaseURL: "https://micropro2.firebaseio.com",
+        projectId: "micropro2",
+        storageBucket: "micropro2.appspot.com",
+        messagingSenderId: "779371700194",
+        appId: "1:779371700194:web:750bcc24e5c05dbd15e3bb",
+        measurementId: "G-BXP5X4WKV1"
+    }
 };
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
 
 
 /***/ }),
